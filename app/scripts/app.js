3554d03e68277e0820ede04fba4cb8f384a6d015
Anton Roolaart <aroolaart@happyfuncorp.com>
2015-01-13 23:03:39 -0500

    fixed many things

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,37 +1,144 @@
 'use strict';
 
 /**
- * @ngdoc overview
- * @name arcmobileApp
+ * @ngdoc function
+ * @name itrsApp.controller:MainCtrl
  * @description
- * # arcmobileApp
- *
- * Main module of the application.
+ * # MainCtrl
+ * Controller of the itrsApp
  */
-angular
-  .module('arcmobileApp', [
+
+
+var app = angular.module('itrsApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
     'ngRoute',
     'ngSanitize',
+    'angularUtils.directives.dirPagination',
     'ngTouch'
   ])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+        controller: 'CarouselCtrl'
       })
       .when('/about', {
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+      .when('/mgmt', {
+        templateUrl: 'views/mgmt.html'
+      })      
+      .when('/services', {
+        templateUrl: 'views/services.html'
+      })      
+      .when('/press', {
+        templateUrl: 'views/press.html'
+      })            
       .when('/inventory', {
         templateUrl: 'views/inventory.html',
         controller: 'InventoryCtrl'
+      })
+      .when('/faq', {
+        templateUrl: 'views/faq.html',
+      })      .when('/contact', {
+        templateUrl: 'views/contact.html',
       })
       .otherwise({
         redirectTo: '/'
       });
   });
+
+  app.config(function(paginationTemplateProvider) {
+    paginationTemplateProvider.setPath('views/dirPagination.tpl.html');
+  });
+
+
+app.controller("InventoryCtrl", function($scope, $http) {
+  $scope.inventory= [];
+  //$http.jsonp('http://www.assetrecovery.com/inventory_json.php?callback=JSON_CALLBACK=?').success(function(data) { 
+  $http.get('data/data.json').success(function(data) {   
+      console.log("success!");
+      $scope.inventory = data.inventory;
+          console.log(data);
+      });    
+});
+
+app.controller("CarouselCtrl", function($scope, $http) {
+  $scope.carousel= [];
+  //$http.jsonp('http://www.assetrecovery.com/inventory_json.php?callback=JSON_CALLBACK=?').success(function(data) { 
+  $http.get('data/carousel.json').success(function(data) {   
+      console.log("success!");
+      $scope.carousel = data.carousel;
+          console.log(data);
+      });    
+});
+
+app.controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
+
+app.controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
+
+// Directive to handle menu highlighting
+app.directive("myDataToggle", function(){
+    function link(scope, element, attrs) {
+        var e = angular.element(element);
+        e.on('click', function(){
+            e.parent().parent().children().removeClass('active');
+            e.parent().addClass("active");
+        })
+    }
+    return {
+        restrict: 'A',
+        link: link
+    };
+});
+
+$(document).ready(function(){
+
+  $("#maincarousel").carousel();
+  $('.navbar-toggle').on('click',function(){
+    $('.container-fluid .share').slideUp("fast");
+    $('.banner .share').css("visibility","visible");      
+  });
+  $(window).on('scroll', function() {
+    var scrollPosition = $(this).scrollTop();
+    if (scrollPosition >= 100 && $('.container-fluid .share').is(":hidden")) {
+        //alert("scrolled");
+        $('.banner .share').css("visibility","hidden");
+        $('.container-fluid .share').slideDown("slow");
+        //$(this).off('scroll');
+    }
+    if (scrollPosition <= 50 && $('.container-fluid .share').is(":visible")) {
+        //alert("scrolled");
+        $('.banner .share').css("visibility","visible");
+        $('.container-fluid .share').slideUp("fast");
+        //$(this).off('scroll');
+    }    
+  });
+  $('.glyphicon-hand-up').on('click',function(){
+     $(this).hide();
+     $('footer .info').show();
+     $('.glyphicon-hand-down').show();
+   });
+   $('.glyphicon-hand-down').on('click',function(){
+     $(this).hide();
+     $('footer .info').hide();
+     $('.glyphicon-hand-up').show();
+   });  
+})
+
+
---
+++ app/scripts/share.js
@@ -0,0 +1,21 @@
+var htmlTitle = $(document).find("title").text();
+//var htmlURL = document.location.href;
+var htmlURL = "http://www.itrecoveryspecialists.com";
+
+var shareURLs = {
+	"site" : [
+		{ 
+			"facebook" : "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(htmlURL) +  "%3Futm_source%3Dfacebook%26utm_medium%3Dsocial%26utm_campaign%3Dshareurlbuttons",
+			"twitter" : "https://twitter.com/intent/tweet?url=" + encodeURIComponent(htmlURL) +  "%3Futm_source%3Dtwitter%26utm_medium%3Dsocial%26utm_campaign%3Dshareurlbuttons&amp;text=" + htmlTitle,
+			"linkedin" : "http://www.linkedin.com/shareArticle?mini=true&url=" + encodeURIComponent(htmlURL) + "&title=" + htmlTitle,
+			"google" : "https://plus.google.com/share?url=" + encodeURIComponent(htmlURL)
+		}
+	]
+}
+
+$(document).ready(function(){
+	$("a[title~='Facebook']").attr("href",shareURLs.site[0].facebook);
+	$("a[title~='Twitter']").attr("href",shareURLs.site[0].twitter);
+	$("a[title~='Linkedin']").attr("href",shareURLs.site[0].linkedin);
+	$("a[title~='Google']").attr("href",shareURLs.site[0].google);
+});
